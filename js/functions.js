1c42e643df96a6e2e69a306ca0fb67af5047f007
gor <gorsayadyan1@gmail.com>
2023-06-16 10:08:08 +0300

    fit: done 3 functions from task

---
+++ js/functions.js
@@ -0,0 +1,47 @@
+// Функция для проверки длины строки. Она принимает строку, которую нужно проверить, и максимальную длину и возвращает true, если строка меньше или равна указанной длине, и false, если строка длиннее. Эта функция нам пригодится для валидации формы. Примеры использования функции:
+
+function stringLength(string, maxLength) {
+  if(string.length <= maxLength) {
+    return true;
+  } else {
+    return false;
+  }
+}
+stringLength('test', 20);
+
+// Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.
+
+function test(string) {
+  let newString = '';
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== ' ') {
+      newString += string[i];
+    }
+  }
+  newString = newString.toLowerCase();
+  const reverseString = newString.split('').reverse().join('');
+  //console.log(reverseString);
+  if (newString === reverseString) {
+    return true;
+  } else {
+    return false;
+  }
+}
+test(); // подошел к решению чуточку иначе (без регулярных выражений, с ними, конечно, полегче. хаха), но вроде работает
+
+
+// Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN:
+
+
+function numbers(string) {
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (!isNaN(Number(string[i]))) {
+      count++;
+    }
+  }
+  return count === 0 ? NaN : parseInt(string.slice(0, count), 10); // не совсем уверен в работоспособности записи
+}
+
+numbers('test213');
+
