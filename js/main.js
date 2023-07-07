af2896fa94cda81d1aab0cbd87e03e98bac0750d
gor <gorsayadyan1@gmail.com>
2023-07-07 10:57:03 +0300

    module 8 task

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
+import { popUpLoad } from './gallery-popup.js';
 import './components.js';
 import { createPictureArray } from './util.js';
 import { renderPictures } from './mini.js';
 renderPictures(createPictureArray());
+console.log(popUpLoad)
