3e12e33d81ff78283254426a7d6db0153af70ad3
gor <gorsayadyan1@gmail.com>
2023-07-21 13:37:39 +0300

    realization getting data from server

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
-import './components.js';
-import { createPictureArray } from './util.js';
+// import './components.js';
+// import { createPictureArray } from './util.js';
 import { createThumbnail } from './mini.js';
 import './upload.js';
+import { request } from './util.js';
 
+/**
+ * @type { Array <createThumbnail>}
+ */
+const data = await request('https://29.javascript.pages.academy/kekstagram/data');
+console.log(data);
 
-createThumbnail(createPictureArray());
+createThumbnail(data);
