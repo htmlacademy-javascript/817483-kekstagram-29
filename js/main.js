407488e2df54b9ace8906965cf76997767203d79
gor <gorsayadyan1@gmail.com>
2023-07-23 20:43:08 +0300

    add error message

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-// import './components.js';
-// import { createPictureArray } from './util.js';
 import { createThumbnail } from './mini.js';
 import './upload.js';
 import { request } from './util.js';
+import { renderStatus } from './status.js';
 
-/**
+try {
+  /**
  * @type { Array <createThumbnail>}
  */
 const data = await request('https://29.javascript.pages.academy/kekstagram/data');
 console.log(data);
 
 createThumbnail(data);
+
+} catch(error) {
+  const title = `Ошибка: ${error.message}`;
+  const button = 'Закрыть';
+
+  renderStatus('error', {title, button});
+}
